3f02c6b8252edae6e9c54bbc85c6f5796f45370c
Hugo Hernani <hhernanni@gmail.com>
2021-04-27 17:10:13 -0300

    Add DI for BatchUploadRepository

--- config/initializers/system.rb
+++ config/initializers/system.rb
@@ -1,6 +1,8 @@
 Dry::Rails.container do
   register(:products_repository, memoize: true){ ApplicationRepository.new(gateway: Product) }
   register(:accounts_repository, memoize: true){ ApplicationRepository.new(gateway: Account) }
+  register(:batch_upload_repository, memoize: true){ BatchUploadRepository.new(gateway: BatchUpload) }
+
   namespace('use_cases') do
     register('authorization', memoize: true){ Authorization.new }
     register('authentication', memoize: true){ Authentication.new }
@@ -8,7 +10,6 @@ Dry::Rails.container do
 
   config.features = %i[
     controller_helpers
-    safe_params
   ]
 
   config.finalize! if Rails.env.production?
