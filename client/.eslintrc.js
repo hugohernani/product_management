1b36dc16ed52bdb6d3be4da27cafdbfe31977b1c
Hugo Hernani <hhernanni@gmail.com>
2021-04-29 15:34:20 -0300

    Remove unused prettier style plugin

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -18,7 +18,6 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   plugins: ['import', 'react-hooks'],
